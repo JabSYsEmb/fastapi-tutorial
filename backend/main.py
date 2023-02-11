11bdd43496ddd0dbb93098f8ca7210da5e521248
JabSysEmb <abduljabbar.sha97@gmail.com>
2023-02-11 16:37:27 +0300

    linting main.py by pylint

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
+"""Module providing utilities functions to deal with JSON files"""
 import json
 
 from enum import Enum
 from pathlib import Path
 
+# from fastapi.responses import HTMLResponse
+# from fastapi.responses import PlainTextResponse
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -31,26 +32,41 @@ app.add_middleware(
 
 # predefined paths using Enum
 class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+  ALEXNET = "alexnet"
+  RESNET = "resnet"
+  LENET = "lenet"
 
 @app.get("/api/v1/models/{model_name}")
-async def root(model_name: ModelName) :
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "has 20k parameters to be tuned"}
-    if model_name.value == ModelName.resnet.value:
-        return {"model_name": model_name, "message": "has 22k parameters to be tuned"}
-    return {"model_name": model_name, "message" : "has 2k parameters to be tuned"}
+async def model_root(model_name: ModelName) :
+  if model_name is ModelName.alexnet:
+    return {
+        "model_name": model_name, 
+        "message": "has 20k parameters to be tuned"
+    }
+  if model_name.value == ModelName.resnet.value:
+    return {
+        "model_name": model_name,
+        "message": "has 22k parameters to be tuned"
+    }
+  return {
+      "model_name": model_name,
+      "message" : "has 2k parameters to be tuned"
+  }
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
 
 @app.get("/api/v1/db/")
-async def root(start: int = 0, length: int = 10):
-    return fake_items_db[ start: start+length ]
+async def db_root(start: int = 0, length: int = 10):
+  return fake_items_db[ start: start+length ]
 
 @app.get("/api/v1/files/{path_to_json_file:path}")
-async def root(path_to_json_file: str):
-    content_path = Path.cwd().resolve().joinpath(path_to_json_file);
-    return {"file": path_to_json_file, "content" : json.loads(content_path.read_text())}
+async def path_root(path_to_json_file: str):
+  content_path = Path.cwd().resolve().joinpath(path_to_json_file)
+  return {
+      "file": path_to_json_file,
+      "content" : json.loads(content_path.read_text(encoding="utf-8"))
+  }
