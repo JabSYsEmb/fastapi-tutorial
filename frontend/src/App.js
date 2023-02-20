86def469d3d396669422b35bdbcaff45eb75c81f
JabSysEmb <abduljabbar.sha97@gmail.com>
2023-02-20 21:07:25 +0300

    frontend work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [isConn, setIsConn] = useState(() => false);
+
   useEffect(() => {
     axios
       .get("http://backend.colasloth.com/api/v1/items")
       .then((res) => res.data)
-      .then((data) => console.log(data));
+      .then((data) => data && setIsConn(true));
   }, []);
-  return <h1>Gello</h1>;
+  return (
+    <div className="app-div">
+      <h1>
+        The Frontend{" "}
+        <span className={isConn ? "green" : "red"}>
+          {isConn ? "is" : "is not"} connected
+        </span>{" "}
+        to the backend
+      </h1>
+    </div>
+  );
 }
 
 export default App;
